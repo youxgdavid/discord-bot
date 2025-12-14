693b606810caa0a54e4ac5147063ccf1bd9ece87
youxgdavid <davidvarady999@gmail.com>
2025-12-14 13:40:22 +0000

    Update Hugging Face API endpoint URL breaking and not generating image

--- main.py
+++ main.py
@@ -1232,7 +1232,7 @@ async def recreate(interaction: discord.Interaction, scene: str):
     await interaction.response.defer(thinking=True)
 
     # Build HF Inference endpoint
-    endpoint = fhttps://router.huggingface.co/hf-inference/models/{HF_MODEL}"
+    endpoint = const API_URL = "https://router.huggingface.co/hf-inference/models/gpt2";
     headers = {"Authorization": f"Bearer {HUGGINGFACE_TOKEN}"}
     payload = {"inputs": scene}
 
